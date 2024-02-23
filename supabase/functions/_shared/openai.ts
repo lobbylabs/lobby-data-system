9e54b41ee9c62abc6e651d8ee446efaf9bd27744
nawage <nlwnawage@gmail.com>
2024-02-23 02:10:44 -0600

    - remove need for partitions - add PATCH and DELETE routes - add documentation diagrams - clean up tokenizer download script - clean up api route organization

--- supabase/functions/_shared/openai.ts
+++ supabase/functions/_shared/openai.ts
@@ -7,55 +7,60 @@ const openai = new OpenAI({
   baseURL: "https://api.lobby-edge.com/v1",
 });
 
-async function getJinaEmbeddings(
-  texts: string[],
-): Promise<number[][]> {
+async function getJinaEmbeddings(texts: string[]): Promise<number[][]> {
   const maxTokens = 4000;
-    // Create the batches
-    const batches = batchTextsByTokenCount(texts, maxTokens, Model.jinaai_jina_embeddings_v2_base_en);
+  // Create the batches
+  const batches = batchTextsByTokenCount(
+    texts,
+    maxTokens,
+    Model.jinaai_jina_embeddings_v2_base_en
+  );
 
-    // Collect embeddings for each batch, maintaining order
-    const allEmbeddings: number[][] = [];
-    for (const batch of batches) {
-      const embeddingResponse = await openai.embeddings.create({
-        input: batch,
-        model: "jina-embeddings-v2-base-en",
-      });
-      allEmbeddings.push(...embeddingResponse.data.map(obj => obj.embedding));
-    }
-  
-    return allEmbeddings;
+  // Collect embeddings for each batch, maintaining order
+  const allEmbeddings: number[][] = [];
+  for (const batch of batches) {
+    const embeddingResponse = await openai.embeddings.create({
+      input: batch,
+      model: "jina-embeddings-v2-base-en",
+    });
+    allEmbeddings.push(...embeddingResponse.data.map((obj) => obj.embedding));
+  }
+
+  return allEmbeddings;
 }
 
-function batchTextsByTokenCount(texts: string[], maxTokens: number, model: Model): string[][] {
-    const batches: string[][] = [];
-    let currentBatch: string[] = [];
-    let currentTokenCount = 0;
-  
-    for (const text of texts) {
-      const textTokenCount = getNumTokens(model, text);
-      if (currentTokenCount + textTokenCount > maxTokens) {
-        // Start a new batch
-        if (currentBatch.length > 0) {
-          batches.push(currentBatch);
-        }
-        currentBatch = [text];
-        currentTokenCount = textTokenCount;
-      } else {
-        // Add to current batch
-        currentBatch.push(text);
-        currentTokenCount += textTokenCount;
+function batchTextsByTokenCount(
+  texts: string[],
+  maxTokens: number,
+  model: Model
+): string[][] {
+  const batches: string[][] = [];
+  let currentBatch: string[] = [];
+  let currentTokenCount = 0;
+
+  for (const text of texts) {
+    const textTokenCount = getNumTokens(model, text);
+    if (currentTokenCount + textTokenCount > maxTokens) {
+      // Start a new batch
+      if (currentBatch.length > 0) {
+        batches.push(currentBatch);
       }
+      currentBatch = [text];
+      currentTokenCount = textTokenCount;
+    } else {
+      // Add to current batch
+      currentBatch.push(text);
+      currentTokenCount += textTokenCount;
     }
-  
-    // Don't forget to add the last batch
-    if (currentBatch.length > 0) {
-      batches.push(currentBatch);
-    }
-  
-    return batches;
   }
-  
+
+  // Don't forget to add the last batch
+  if (currentBatch.length > 0) {
+    batches.push(currentBatch);
+  }
+
+  return batches;
+}
 
 // Export the configured OpenAI client
 export { openai, getJinaEmbeddings };
--- supabase/functions/data-api/v1/documents/index.ts
+++ supabase/functions/data-api/v1/documents/index.ts
@@ -1,6 +1,5 @@
 import { Router, Status } from "oak";
-import { chunksRouter } from "./chunks/index.ts";
-import { getChunks } from "../../../../../_shared/utils.ts";
+import { getChunks } from "../../../_shared/utils.ts";
 
 const documentsRouter = new Router();
 
@@ -30,37 +29,61 @@ documentsRouter
     const { text, title, source_url } = await context.request.body.json();
     // console.log(text, title, source_url, orgId, botId);
 
-    const chunks = await getChunks(text, 200);
     const { data: documentData, error: documentError } =
-      await context.state.sbclient.rpc("create_document_with_chunks", {
+      await context.state.sbclient.rpc("create_document", {
         p_bot_id: botId,
         p_organization_id: orgId,
         p_title: title,
         p_source_url: source_url,
         p_document_type: "text",
-        p_chunks_info: chunks,
       });
 
     if (documentError) {
       context.response.status = Status.BadRequest;
       context.response.body = { error: documentError };
+    }
+
+    const chunks = await getChunks(text, 200);
+    console.log(chunks.length)
+    console.log("GOT CHUNKS!")
+    const { data: documentChunkData, error: documentChunkError } =
+      await context.state.sbclient.rpc("create_document_chunks", {
+        p_document_id: documentData[0].id,
+        p_bot_id: botId,
+        p_organization_id: orgId,
+        p_chunks_info: chunks,
+      });
+
+    if (documentChunkError) {
+      context.response.status = Status.BadRequest;
+      context.response.body = { error: documentChunkError };
+    }
+
+    const { data: newDocumentData, error: newDocumentError } =
+      await context.state.sbclient.rpc("get_document", {
+        p_bot_id: botId,
+        p_organization_id: orgId,
+        p_document_id: documentData[0].id,
+      });
+
+    if (newDocumentError) {
+      context.response.status = Status.BadRequest;
+      context.response.body = { error: newDocumentError };
     } else {
       context.response.status = Status.OK;
-      context.response.body = { data: documentData };
+      context.response.body = { data: newDocumentData };
     }
   })
   .get("/:docId", (context) => {
     context.response.status = Status.NotImplemented;
     context.response.body = `Fetching document with ID: ${context.params.docId}`;
   })
-  .put("/:docId", (context) => {
+  .patch("/:docId", (context) => {
     context.response.status = Status.NotImplemented;
     context.response.body = `Updating document with ID: ${context.params.docId}`;
   })
   .delete("/:docId", (context) => {
     context.response.status = Status.NotImplemented;
     context.response.body = `Deleting document with ID: ${context.params.docId}`;
-  })
-  .use("/:docId/chunks", chunksRouter.routes(), chunksRouter.allowedMethods());
-
+  });
 export { documentsRouter };
